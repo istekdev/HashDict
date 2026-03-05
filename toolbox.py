c4d7bd46adec7dc47dc40c86dbd9ba719eb2d9b4
ISTEK <200693214+istekdev@users.noreply.github.com>
2026-03-04 20:26:18 -0600

    Update toolbox.py

--- toolbox.py
+++ toolbox.py
@@ -1,4 +1,39 @@
+from pathlib import Path
 import json, copy
+
+def parse(name):
+  if name == "dictionary":
+    with open(f"{Path(__file__).parent}/{config['directories']['datasets']['dictionary']['english']}", "r") as r:
+      en = json.load(r)
+    with open(f"{Path(__file__).parent}/{config['directories']['datasets']['dictionary']['romanian']}", "r") as r:
+      ro = json.load(r)
+    with open(f"{Path(__file__).parent}/{config['directories']['datasets']['dictionary']['portuguese']}", "r") as r:
+      pt = json.load(r)
+    with open(f"{Path(__file__).parent}/{config['directories']['datasets']['dictionary']['spanish']}", "r") as r:
+      es = json.load(r)
+    with open(f"{Path(__file__).parent}/{config['directories']['datasets']['dictionary']['chinese']}", "r") as r:
+      zh = json.load(r)
+    with open(f"{Path(__file__).parent}/{config['directories']['datasets']['dictionary']['italian']}", "r") as r:
+      it = json.load(r)
+    with open(f"{Path(__file__).parent}/{config['directories']['datasets']['dictionary']['french']}", "r") as r:
+      fr = json.load(r)
+    return en + ro + pt + es + zh + it + fr
+  elif name == "dataBreach":
+    with open(f"{Path(__file__).parent}/{config['directories']['datasets']['dataBreaches']['crackedHashes']}", "r") as r:
+      ch = json.load(r)
+    with open(f"{Path(__file__).parent}/{config['directories']['datasets']['dataBreaches']['usernames']}", "r") as r:
+      us = json.load(r)
+    with open(f"{Path(__file__).parent}/{config['directories']['datasets']['dataBreaches']['passwords']['part1']}", "r") as r:
+      p1 = json.load(r)
+    with open(f"{Path(__file__).parent}/{config['directories']['datasets']['dataBreaches']['passwords']['part2']}", "r") as r:
+      p2 = json.load(r)
+    with open(f"{Path(__file__).parent}/{config['directories']['datasets']['dataBreaches']['passwords']['part3']}", "r") as r:
+      p3 = json.load(r)
+    with open(f"{Path(__file__).parent}/{config['directories']['datasets']['dataBreaches']['passwords']['part4']}", "r") as r:
+      p4 = json.load(r)
+    return ch + us + p1 + p2 + p3 + p4
+  else:
+    return False
 
 def template(path):
   with open(path, "r") as r:
