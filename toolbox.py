f15156748cfe9fc8a58a873003c264205c8e3918
ISTEK <200693214+istekdev@users.noreply.github.com>
2026-03-04 19:58:48 -0600

    Create toolbox.py

---
+++ toolbox.py
@@ -0,0 +1,18 @@
+def byte(inp):
+  hexadecimal = None
+  try:
+    bytes.fromhex(inp)
+    hexadecimal = True
+  except:
+    hexadecimal = False
+
+  if isinstance(inp, int):
+    return inp.to_bytes((inp.bit_length() + 7) // 8, "big")
+  elif isinstance(inp, str) and hexadecimal == False:
+    return inp.encode("utf-8")
+  elif isinstance(inp, bool) or inp == None:
+    return str(inp).encode("utf-8")
+  elif isinstance(inp, list):
+    return b"".join(inp)
+  elif hexadecimal:
+    return bytes.fromhex(inp)
